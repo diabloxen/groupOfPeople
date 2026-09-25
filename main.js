e0b0a35e821c0bd066c628448ac15eab9526960e
michael <m.anyaegbuna@rgu.ac.uk>
2026-09-25 17:05:16 +0000

    init the project, added .gitignore and updated main.js to include a basic boilerplate for the questions

---
+++ main.js
@@ -0,0 +1,102 @@
+const prompt = require('readline-sync');
+const categories =
+[
+    {
+        name : generalKnowledge,
+        questions : [
+            {
+                question : 'what is the boiling point of water?',
+                answers : [ '100', '200', '300', '400' ],
+                correctAnswer : 0
+            },
+            {
+                question : 'what is the freezing point of water?',
+                answers : [ '0', '10', '20', '30' ],
+                correctAnswer : 0
+            },
+            {
+                question : 'What company was initially known as "Blue Ribbon Sports"?  ',
+                answers : [ 'Nike', 'Adidas', 'Puma', 'Reebok' ],
+                correctAnswer : 0
+            }
+        ]
+    },
+    {
+        name : filmTv,
+        questions : 
+        [
+            { question :  ''   ,
+                answers : [],
+                correctAnswer : lorem
+            }
+        ]
+    },
+
+   {
+        name : food,
+        questions : 
+        [
+            { question :  ''   ,
+                answers : [],
+                correctAnswer : lorem
+            }
+        ]
+    },
+
+    {
+        name : math,
+        questions : 
+        [
+            {
+                question : 'what is a 2 + 2?',
+                answers : [ '2', '3', '4', '5' ],
+                correctAnswer : 2
+            }
+        ]
+    },
+
+    {
+        name : music,
+        questions : 
+        [
+            { question :  ''   ,
+                answers : [],
+                correctAnswer : lorem
+            }
+        ]
+    },
+
+    {
+        name : sports,
+        questions : 
+        [
+            { question :  ''   ,
+                answers : [],
+                correctAnswer : lorem
+            }
+        ]
+    },
+
+    {
+        name : geography,
+        questions : 
+        [
+            { question :  ''   ,
+                answers : [],
+                correctAnswer : lorem
+            }
+        ]
+    },
+
+    {
+        name : animals,
+        questions : 
+        [
+            { question :  ''   ,
+                answers : [],
+                correctAnswer : lorem
+            }
+        ]
+    },
+] 
+
